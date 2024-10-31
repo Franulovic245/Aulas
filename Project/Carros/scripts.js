b301de85cd042303e8de69c5c30e0979c62b6471
Igor Franulovic <igorfranulovic09@gmail.com>
2024-10-31 18:03:13 -0300

    project car

---
+++ Project/Carros/scripts.js
@@ -0,0 +1,42 @@
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let items = container.querySelectorAll('.list .item')
+let indicator = document.querySelector('.indicators')
+let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list')
+
+
+let active = 0
+let firstposition = 0
+let lastposition = items.length - 1
+
+function pressbutton() {
+
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    let dotsOld = indicator.querySelector('ul li.active')
+        dotsOld.classList.remove('active')
+        dots[active].classList.add('active')
+
+    indicator.querySelector('.number').innerHTML = '0' + (active + 1)
+
+}
+
+prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+    active = active + 1 > lastposition ? 0 : active + 1
+    pressbutton()
+    items[active].classList.add('active')
+
+   
+}
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
+    active = active - 1 < firstposition ? lastposition : active - 1
+    pressbutton()
+    items[active].classList.add('active')
+
+}
